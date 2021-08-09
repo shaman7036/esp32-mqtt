ccb8da6615db2958814b78f635c4b6c5724484eb
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-09 17:48:50 -0300

    added files
    - create project
    - setup docker

--- web/ofc_integracao/settings.py
+++ web/ofc_integracao/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3k4k%ek7ace!ax0o2g6ru^j7&8v-+$mkcs9r&72jp7x(epj-_&'
+SECRET_KEY = 'django-insecure-12_#f_c#2a66+_2kr)^60rv2k5df+*5#-iojwi^f3+ekfehj*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
