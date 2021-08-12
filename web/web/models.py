2cea3c38e307f3a57d7213642ee7e0187179522b
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-12 09:01:53 -0300

    add django project
    - docker
    - saves to the database

---
+++ web/web/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
+
+class Temperature(BaseModel):
+    value = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=5,
+    )
+
+class Humidity(BaseModel):
+    value = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=5,
+    )
+
+class Pressure(BaseModel):
+    value = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=6,
+    )
+
+class Co2(BaseModel):
+    value = models.IntegerField(
+        blank=False,
+    )
+
+class Tvoc(BaseModel):
+    value = models.IntegerField(
+        blank=False,
+    )
---
+++ web/web/mqtt.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime, timezone, timedelta
+import paho.mqtt.client as mqtt
+
+TEMP = "school-monitor/sensor/bme280-temperature/state"
+HUMIDITY = "school-monitor/sensor/bme280-humidity/state"
+PRESSURE = "school-monitor/sensor/bme280-pressure/state"
+CO2 = "school-monitor/sensor/ccs811-co2/state"
+TVOC = "school-monitor/sensor/ccs811-tvoc/state"
+
+MQTT_TOPIC = [(TEMP, 0), (HUMIDITY, 0),
+              (PRESSURE, 0), (CO2, 0),
+              (TVOC, 0)]
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    from .models import Temperature, Humidity, Pressure, Tvoc, Co2
+
+    message_dict = json.loads(msg.payload)
+
+    if msg.topic == TEMP:
+        Temperature(value=message_dict).save()
+        #print("temperature saved")
+    elif msg.topic == HUMIDITY:
+        Humidity(value=message_dict).save()
+    elif msg.topic == PRESSURE:
+        Pressure(value=message_dict).save()
+    elif msg.topic == CO2:
+        Co2(value=message_dict).save()
+    elif msg.topic == TVOC:
+        Tvoc(value=message_dict).save()
+
+client = mqtt.Client("django_paho_mqtt_subscriber")
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("test.mosquitto.org", 1883)
+
