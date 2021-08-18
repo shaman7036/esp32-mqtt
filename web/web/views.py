78927866ff3ce11158547213068d04e59bd1813a
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-18 17:56:45 -0300

    add graphs.js
    todo
    - add timestamps in graph
    - add graphs for humidity, pressure, co2 and tvoc
    - make it look decent

--- web/web/views.py
+++ web/web/views.py
@@ -1,3 +1,21 @@
+import json
 from django.shortcuts import render
+from .models import Temperature, Humidity, Pressure, Tvoc, Co2
 
-# Create your views here.
+def temperature(request):
+    queryset = Temperature.objects.all()
+    values = [int(obj.value) for obj in queryset]
+
+    context = {
+        'values': json.dumps(values),
+    }
+    return render(request, 'web/temperature.html', context)
+
+#def humidity(request):
+#    queryset = Humidity.objects.all()
+#    values = [int(obj.value) for obj in queryset]
+
+#    context = {
+#        'values': json.dumps(values),
+#    }
+#    return render(request, 'web/humidity.html', context)
