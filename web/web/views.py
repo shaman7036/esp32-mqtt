41de5950acc59e89e5db2a5f7a471f4b4a000624
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-19 09:28:05 -0300

    add multiple graphs

--- web/web/views.py
+++ web/web/views.py
@@ -3,19 +3,22 @@ from django.shortcuts import render
 from .models import Temperature, Humidity, Pressure, Tvoc, Co2
 
 def temperature(request):
-    queryset = Temperature.objects.all()
-    values = [int(obj.value) for obj in queryset]
+    queryset_temp = Temperature.objects.all()
+    temp_values = [int(obj.value) for obj in queryset_temp]
+    queryset_humi = Humidity.objects.all()
+    humi_values = [int(obj.value) for obj in queryset_humi]
+    queryset_press = Pressure.objects.all()
+    press_values = [int(obj.value) for obj in queryset_press]
+    queryset_co2 = Co2.objects.all()
+    press_co2 = [int(obj.value) for obj in queryset_co2]
+    queryset_tvoc = Tvoc.objects.all()
+    press_tvoc = [int(obj.value) for obj in queryset_tvoc]
 
     context = {
-        'values': json.dumps(values),
+        'temp_values': json.dumps(temp_values),
+        'humi_values': json.dumps(humi_values),
+        'press_values': json.dumps(press_values),
+        'co2_values': json.dumps(press_co2),
+        'tvoc_values': json.dumps(press_tvoc),
     }
     return render(request, 'web/temperature.html', context)
-
-#def humidity(request):
-#    queryset = Humidity.objects.all()
-#    values = [int(obj.value) for obj in queryset]
-
-#    context = {
-#        'values': json.dumps(values),
-#    }
-#    return render(request, 'web/humidity.html', context)
