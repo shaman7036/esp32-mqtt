7dfa80400bdd743453b7365da869b9ed3d6f3acd
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-26 20:01:25 -0300

    fix database bug
    - saves to de database once a minute as expected

--- web/ofc_integracao/urls.py
+++ web/ofc_integracao/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from web import views
 
+from web import mqtt
+mqtt.run_client()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('temperature/', views.temperature, name='temperature'),
--- web/web/mqtt.py
+++ web/web/mqtt.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime, timezone, timedelta
 import paho.mqtt.client as mqtt
 
 TEMP = "school-monitor/sensor/bme280-temperature/state"
@@ -8,38 +7,44 @@ PRESSURE = "school-monitor/sensor/bme280-pressure/state"
 CO2 = "school-monitor/sensor/ccs811-co2/state"
 TVOC = "school-monitor/sensor/ccs811-tvoc/state"
 
-MQTT_TOPIC = [(TEMP, 0), (HUMIDITY, 0),
+MQTT_TOPICS = [(TEMP, 0), (HUMIDITY, 0),
               (PRESSURE, 0), (CO2, 0),
               (TVOC, 0)]
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(MQTT_TOPIC)
+    print("CONNACK received with code %d." % (rc))
+    client.subscribe(MQTT_TOPICS)
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
     from .models import Temperature, Humidity, Pressure, Tvoc, Co2
 
-    message_dict = json.loads(msg.payload)
-
     print(msg.retain)
 
-    if msg.retain == False:
-        if msg.topic == TEMP:
-            Temperature(value=message_dict).save()
-            print("temperature saved")
-        elif msg.topic == HUMIDITY:
-            Humidity(value=message_dict).save()
-            print("humidity saved")
-        elif msg.topic == PRESSURE:
-            Pressure(value=message_dict).save()
-            print("pressure saved")
-        elif msg.topic == CO2:
-            Co2(value=message_dict).save()
-            print("co2 saved")
-        elif msg.topic == TVOC:
-            Tvoc(value=message_dict).save()
-            print("tvoc saved")
+    message_dict = json.loads(msg.payload)
 
-client = mqtt.Client(client_id="django_paho_mqtt_subscriber", clean_session=False, userdata=None)
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("test.mosquitto.org", 1883)
+    if msg.topic == TEMP:
+        Temperature(value=message_dict).save()
+        print("temperature saved")
+    elif msg.topic == HUMIDITY:
+        Humidity(value=message_dict).save()
+        print("humidity saved")
+    elif msg.topic == PRESSURE:
+        Pressure(value=message_dict).save()
+        print("pressure saved")
+    elif msg.topic == CO2:
+        Co2(value=message_dict).save()
+        print("co2 saved")
+    elif msg.topic == TVOC:
+        Tvoc(value=message_dict).save()
+        print("tvoc saved")
+
+def run_client():
+    client = mqtt.Client("django_paho_mqtt_subscriber", clean_session=True, userdata=None)
+    client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.connect_async(host="test.mosquitto.org", port=1883, keepalive=60, bind_address="")
+    client.loop_start()
